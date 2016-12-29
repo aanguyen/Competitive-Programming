004f858f0b899cb00ae00ef7f3a1080f994ed62c
Andrew Nguyen <andrew.nguyen.waterloo@gmail.com>
2016-12-29 02:29:46 -0500

    Add files via upload

---
+++ CCC/CCC_2011_J5.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class CCC_2011_J5 {
+	public static Scanner kbd = new Scanner(System.in);
+	
+	public static void main (String[] args) {
+		int people = kbd.nextInt();
+		int[] invite = new int[people];
+		for (int i = 1; i < people; i++)
+			invite[i] = kbd.nextInt();
+		System.out.println(ways(invite, people, people));
+	}
+	public static int ways(int[] invite, int people, int num) {
+		int ans = 1;
+		for (int i = 0; i < people; i++)
+			if (invite[i]==num)
+				ans *= (1+ways(invite, number, i+1));
+		return ans;
+	}
+}
