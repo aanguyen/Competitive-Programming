e3051d67fa6a84c56ee8c7d964c643a494c96fb4
Andrew Nguyen <andrew.nguyen.waterloo@gmail.com>
2016-12-30 00:20:37 -0500

    Add files via upload

---
+++ CCC/CCC_2013_J1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CCC_2013_J1 {
+	public static Scanner kbd = new Scanner(System.in);
+	
+	public static void main (String[] args) {
+		int a = kbd.nextInt(), b = kbd.nextInt();
+		System.out.println(b+(b-a));
+	}
+}
